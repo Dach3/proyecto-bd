4fd6d40b9cbff48d221ce699daf48aec9f439a48
Dache95 <jparedes123469@gmail.com>
2022-12-19 10:41:42 -0600

    update

--- formulario_categoria.php
+++
@@ -1,7 +0,0 @@
-<fieldset>
-    <legend>Información General</legend>
-
-    <label for="nombre">Nombre:</label>
-    <input type="text" id="nombre" name="categoria[nombre]" placeholder="Nombre categoria" value="<?php echo s( $categoria->nombre ); ?>">
-
-</fieldset>
